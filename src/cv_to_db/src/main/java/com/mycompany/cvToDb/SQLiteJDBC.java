4bc0fa14dfe3ba2e9eef25c97060c33df4579935
Axel-Egide Hounsou <egide95@gmail.com>
2019-09-03 14:37:28 -0400

    set auto commit to off and added bacth insertion for optimization

--- src/cv_to_db/src/main/java/com/mycompany/cvToDb/SQLiteJDBC.java
+++ src/cv_to_db/src/main/java/com/mycompany/cvToDb/SQLiteJDBC.java
@@ -8,16 +8,19 @@ public class SQLiteJDBC {
    private Connection conn = null;
    private final String PATHTODB = "." + File.separator + "db" + File.separator;
    private String dbname;
+   private PreparedStatement pstmt;
+   private int batchUpperLimit = 1;
+   private int currentBatch = 0;
 
    SQLiteJDBC(String dbname) {
 
       this.dbname = dbname;
    }
 
-   public void connect(){
+   public void connect() {
 
       try {
-       
+
          String dbPath = PATHTODB + dbname + ".db";
          Class.forName("org.sqlite.JDBC");
 
@@ -30,6 +33,7 @@ public class SQLiteJDBC {
          System.exit(0);
       }
    }
+
    public Boolean createTable(String tablename) {
 
       String sql = "CREATE TABLE IF NOT EXISTS " + tablename + "(\n" + "    id integer PRIMARY KEY,\n"
@@ -47,29 +51,78 @@ public class SQLiteJDBC {
       return true;
    }
 
-   public Boolean addToDB(String tablename,  String[] data) {
-
+   /**
+    * Create a prepare statement for batch insertion into database
+    and set auto commit to false
+    * 
+    * @param tablename name of table
+    * 
+    */
+   public void prepareForTable(String tablename) {
       String sql = "INSERT INTO " + tablename + " (A,B,C,D,E,F,G,H,I,J) VALUES(?,?,?,?,?,?,?,?,?,?)";
+      try {
+         pstmt = conn.prepareStatement(sql);
+         conn.setAutoCommit(false);
 
-      try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-         pstmt.setString(1, data[0]);
-         pstmt.setString(2, data[1]);
-         pstmt.setString(3, data[2]);
-         pstmt.setString(4, data[3]);
-         pstmt.setString(5, data[4]);
-         pstmt.setString(6, data[5]);
-         pstmt.setString(7, data[6]);
-         pstmt.setString(8, data[7]);
-         pstmt.setString(9, data[8]);
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }
+   }
+
+   public void batchInsertion(String[] data, int limit) {
+
+      if (batchUpperLimit < limit)
+         batchUpperLimit = limit;
+
+      try {
+         pstmt.setString(1,  data[0]);
+         pstmt.setString(2,  data[1]);
+         pstmt.setString(3,  data[2]);
+         pstmt.setString(4,  data[3]);
+         pstmt.setString(5,  data[4]);
+         pstmt.setString(6,  data[5]);
+         pstmt.setString(7,  data[6]);
+         pstmt.setString(8,  data[7]);
+         pstmt.setString(9,  data[8]);
          pstmt.setString(10, data[9]);
 
-         pstmt.executeUpdate();
-      } catch (SQLException e) {
-         System.out.println(e.getMessage());
-         return false;
-      }
+         pstmt.addBatch();
 
+         currentBatch++;
+
+         /* add to db if bactch upper limit is reach and reset currentBatch */
+         if (currentBatch == batchUpperLimit) {
+            pstmt.executeBatch();
+            conn.commit();
+            currentBatch = 0;
+         }
+
+      } catch (SQLException e) {
+         System.err.println(e.getMessage());
+    
+      }
+   }
+
+   /**
+    * Insert the rest of the data that 
+      are still in the batch
+    */
+   public void flushBatch(){
+
+      try {
+         pstmt.executeBatch();
+         conn.commit();
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }
       
-      return true;
+   }
+
+   public void closeConn(){
+      try {
+         conn.close();
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }
    }
 }
